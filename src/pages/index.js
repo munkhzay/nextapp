1cbcfcf9e8e121505d07f5bcdb6ad279c8969241
muuuza <muzaankhbayr@gmail.com>
2024-08-14 10:19:32 +0800

    shine.next gitrvv pushlej vzew

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,9 +61,7 @@ export default function Home() {
             <h2>
               Learn <span>-&gt;</span>
             </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
+      
           </a>
 
           <a
