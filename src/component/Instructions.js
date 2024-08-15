75d131fa15a40b581e6a43e3ed390388f27d242f
muuuza <muzaankhbayr@gmail.com>
2024-08-15 22:44:43 +0800

    mapiig jijigrvvlen hiw:gert

--- src/component/Instructions.js
+++ src/component/Instructions.js
@@ -1,53 +1,53 @@
-const steps = [
-  { text: "Preheat the oven to 350 degrees." },
-  { text: "Spread the olive oil around a glass baking dish." },
-  { text: "Add the salmon, garlic, and pine nuts to the dish." },
-  { text: "Bake for 15 minutes." },
-  { text: "Add the yellow squash and put back in the oven for 30 mins." },
-  {
-    text: "Remove from oven and let cool for 15 minutes. Add the lettuce and serve.",
-  },
-];
-const Name = (props) => {
-  const { text } = props;
-  return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );
-};
-export const SalmonInstruction = () => {
-  return (
-    <div>
-      {steps.map((step, index) => {
-        return <Name key={index} text={step?.text}></Name>;
-      })}
-    </div>
-  );
-};
+// const steps = [
+//   { text: "Preheat the oven to 350 degrees." },
+//   { text: "Spread the olive oil around a glass baking dish." },
+//   { text: "Add the salmon, garlic, and pine nuts to the dish." },
+//   { text: "Bake for 15 minutes." },
+//   { text: "Add the yellow squash and put back in the oven for 30 mins." },
+//   {
+//     text: "Remove from oven and let cool for 15 minutes. Add the lettuce and serve.",
+//   },
+// ];
+// const Name = (props) => {
+//   const { text } = props;
+//   return (
+//     <div>
+//       <p>{text}</p>
+//     </div>
+//   );
+// };
+// export const SalmonInstruction = () => {
+//   return (
+//     <div>
+//       {steps.map((step, index) => {
+//         return <Name key={index} text={step?.text}></Name>;
+//       })}
+//     </div>
+//   );
+// };
 
-//
+// //
 
-const fishsteps = [
-  { text: "Cook the fish on the grill until hot." },
-  { text: "Place the fish on the 3 tortillas." },
-  { text: "Top them with lettuce, tomatoes, and cheese" },
-];
-const List = (props) => {
-  const { text } = props;
-  console.log(text);
-  return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );
-};
-export const Fishinstruction = () => {
-  return (
-    <div>
-      {fishsteps?.map((fishstep, index) => {
-        return <List text={fishstep?.text} key={index}></List>;
-      })}
-    </div>
-  );
-};
+// const fishsteps = [
+//   { text: "Cook the fish on the grill until hot." },
+//   { text: "Place the fish on the 3 tortillas." },
+//   { text: "Top them with lettuce, tomatoes, and cheese" },
+// ];
+// const List = (props) => {
+//   const { text } = props;
+//   console.log(text);
+//   return (
+//     <div>
+//       <p>{text}</p>
+//     </div>
+//   );
+// };
+// export const Fishinstruction = () => {
+//   return (
+//     <div>
+//       {fishsteps?.map((fishstep, index) => {
+//         return <List text={fishstep?.text} key={index}></List>;
+//       })}
+//     </div>
+//   );
+// };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,102 @@
-import { Salmon, Fish } from "@/component/recipes";
+// import { Salmon, Fish } from "@/component/recipes";
 // import { SalmonInstruction, Fishinstruction } from "@/component/Instructions";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Deliciuos recipes</h1>
+//       <h2>Baked salmon</h2>
+//       <Salmon></Salmon>
+//       <h1>Cooking Instruction</h1>
+//       <SalmonInstruction></SalmonInstruction>
+//       <h2>Fish Tacos</h2>
+//       <Fish></Fish>
+//       <h1>Cooking Instruction</h1>
+//       <Fishinstruction></Fishinstruction>
+//     </div>
+//   );
+// }
+
+const datas = [
+  {
+    name: "Baked Salmon",
+    ingredients: [
+      { name: "Salmon", amount: 1, measurement: "l lb" },
+      { name: "Pine Nuts", amount: 1, measurement: "cup" },
+      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
+      { name: "Yellow Squash", amount: 1, measurement: "med" },
+      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
+      { name: "Garlic", amount: 3, measurement: "cloves" },
+    ],
+    steps: [
+      "Preheat the oven to 350 degrees.",
+      "Spread the olive oil around a glass baking dish.",
+      "Add the salmon, garlic, and pine nuts to the dish.",
+      "Bake for 15 minutes.",
+      "Add the yellow squash and put back in the oven for 30 mins.",
+      "Remove from oven and let cool for 15 minutes. Add the lettuce and serve.",
+    ],
+  },
+  {
+    name: "Fish Tacos",
+    ingredients: [
+      { name: "Whitefish", amount: 1, measurement: "l lb" },
+      { name: "Cheese", amount: 1, measurement: "cup" },
+      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
+      { name: "Tomatoes", amount: 2, measurement: "large" },
+      { name: "Tortillas", amount: 3, measurement: "med" },
+    ],
+    steps: [
+      "Cook the fish on the grill until hot.",
+      "Place the fish on the 3 tortillas.",
+      "Top them with lettuce, tomatoes, and cheese",
+    ],
+  },
+];
+
+const List = (props) => {
+  const { food } = props;
   return (
     <div>
-      <h1>Deliciuos recipes</h1>
-      <h2>Baked salmon</h2>
-      <Salmon></Salmon>
-      <h1>Cooking Instruction</h1>
-      <SalmonInstruction></SalmonInstruction>
-      <h2>Fish Tacos</h2>
-      <Fish></Fish>
-      <h1>Cooking Instruction</h1>
-      <Fishinstruction></Fishinstruction>
+      {" "}
+      <li>{food}</li>
     </div>
   );
-}
-
-// const datas = [
-//   {
-//     name: "Baked Salmon",
-//     ingredients: [
-//       { name: "Salmon", amount: 1, measurement: "l lb" },
-//       { name: "Pine Nuts", amount: 1, measurement: "cup" },
-//       { name: "Butter Lettuce", amount: 2, measurement: "cups" },
-//       { name: "Yellow Squash", amount: 1, measurement: "med" },
-//       { name: "Olive Oil", amount: 0.5, measurement: "cup" },
-//       { name: "Garlic", amount: 3, measurement: "cloves" },
-//     ],
-//     steps: [
-//       "Preheat the oven to 350 degrees.",
-//       "Spread the olive oil around a glass baking dish.",
-//       "Add the salmon, garlic, and pine nuts to the dish.",
-//       "Bake for 15 minutes.",
-//       "Add the yellow squash and put back in the oven for 30 mins.",
-//       "Remove from oven and let cool for 15 minutes. Add the lettuce and serve.",
-//     ],
-//   },
-//   {
-//     name: "Fish Tacos",
-//     ingredients: [
-//       { name: "Whitefish", amount: 1, measurement: "l lb" },
-//       { name: "Cheese", amount: 1, measurement: "cup" },
-//       { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
-//       { name: "Tomatoes", amount: 2, measurement: "large" },
-//       { name: "Tortillas", amount: 3, measurement: "med" },
-//     ],
-//     steps: [
-//       "Cook the fish on the grill until hot.",
-//       "Place the fish on the 3 tortillas.",
-//       "Top them with lettuce, tomatoes, and cheese",
-//     ],
-//   },
-// ];
-
-// const List = (props) => {
-//   const { name } = props;
-//   return (
-//     <div>
-//       {" "}
-//       <li>{name}</li>
-//     </div>
-//   );
-// };
-
-// const Step = (props) => {
-//   const { step } = props;
-//   return (
-//     <div>
-//       <p>{step}</p>
-//     </div>
-//   );
-// };
-// const Recipe = () => {
-//   return (
-//     <div>
-//       {datas.map((data) => {
-//         return;
-//         <List name={data.name}></List>;
-//         {
-//           ingredients.map((ingredient, index) => {
-//             return (
-//               <div>
-//                 <List key={index} name={ingredient.name}></List>
-//                 <Step key={index} step={steps.value}></Step>
-//               </div>
-//             );
-//           });
-//         }
-//       })}
-//     </div>
-//   );
-// };
-// export default Recipe;
+};
+const Head = (props) => {
+  const { para } = props;
+  return (
+    <div>
+      <h1>{para}</h1>
+    </div>
+  );
+};
+const Step = (props) => {
+  const { step } = props;
+  console.log(step);
+  return (
+    <div>
+      <p>{step}</p>
+    </div>
+  );
+};
+const Recipe = () => {
+  return (
+    <div>
+      {datas.map((data) => {
+        return (
+          <div>
+            <Head para={data.name}></Head>
+            {data.ingredients.map((ingredient, index) => {
+              return <List key={index} food={ingredient.name}></List>;
+            })}
+            {data.steps.map((step, index) => {
+              return <Step key={index} step={step}></Step>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Recipe;
